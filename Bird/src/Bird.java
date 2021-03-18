0686acf13b55e4c21114061d294900c1ca00bf2b
Josep <Josep@192.168.100.37>
2021-03-18 11:08:24 +0300

    First comment

---
+++ Bird/src/Bird.java
@@ -0,0 +1,16 @@
+
+public class Bird {
+
+	public static void main(String[] args) {
+		
+		String color = "Blue";
+		
+	}
+	public static void fly() {
+		System.out.println("Birds can fly");
+	}
+	
+	public static void tweet() {
+		System.out.println("Birds can tweet");
+	}
+}
---
+++ Bird/src/Parrot.java
@@ -0,0 +1,8 @@
+
+public class Parrot extends Bird {
+
+	public static void talk() {
+		System.out.println("The Parrot is a bird");
+	}
+	
+}
